7bdaf74ef705f41f897b2424dfacf077141fb08d
Alin Corodescu <alin.corodescu@gmail.com>
2019-11-07 20:51:19 +0100

    finishing touches

---
+++ PeerSim/delivery/data/tools/DataExtractor.py
@@ -0,0 +1,45 @@
+# control.graphPL: 0.999 1.8999099909991
+# This script extracts data from the simulations output files (e.g. output_star_c30.txt) and generates separate
+# files for each property: clustering coefficient, path length and in-degree.
+
+import re
+import sys
+
+if __name__ == "__main__":
+    f  = open(sys.argv[1], "r")
+    lines = f.readlines()
+    
+    layout = sys.argv[2]
+    c = sys.argv[3]
+
+    clustering_file = "clustering_" + layout + "_c" + c 
+    path_len_file = "path_len_" + layout + "_c" + c
+    in_deg_file = "in_deg_" + layout + "_c" + c
+
+    # clustering coefficient file
+    c_file = open(clustering_file, "w")
+    # path length file
+    p_file = open(path_len_file, "w")
+    # in-degree file
+    i_file = open(in_deg_file, "w")
+
+    cycle = 0
+    for line in lines:
+        if re.match("control\.graphPL: (.*) (.*)", line):
+            match = re.search("control\.graphPL: (.*) (.*)", line)
+            coeff = match.group(1)
+            average = match.group(2)
+
+            c_file.write(str(cycle) + " " + coeff + '\n')
+            p_file.write(str(cycle) + " " + average + '\n')
+
+            cycle = cycle + 1
+        elif re.match("([0-9]+) ([0-9]+)", line):
+            i_file.write(line + '\n')
+
+    c_file.close()
+    p_file.close()
+    i_file.close()
+
+
+
---
+++ PeerSim/delivery/jar/README.md
@@ -0,0 +1,3 @@
+How to run:
+
+java -jar PeerSim.jar path/to/simulation/script
